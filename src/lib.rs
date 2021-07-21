8bfa87bda347b5b98e234acefe4144a50c49fe0d
miam-miam100 <49870539+miam-miam100@users.noreply.github.com>
2021-07-21 19:27:04 +0100

    Added Tests for Add.

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,6 @@ pub mod types;
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::types::{MathError, Token};
 
     #[test]
     fn tokenise() {
@@ -110,6 +109,36 @@ mod tests {
                 num: 1,
                 den: 2,
             }
+        );
+    }
+
+    #[test]
+    fn add() {
+        let mut expr = expression::Expression::new("5+1-5.1+2-5.1+9.5-5.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001+9-5.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001+5.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001-7.1+5.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001");
+        assert_eq!(expr.tokenise(), types::MathError::None);
+        assert_eq!(
+            Ok(types::Token::Integer(6)),
+            number::add(expr.infix_token[0], expr.infix_token[2])
+        );
+        assert_eq!(
+            Ok(types::Token::Fraction(types::Fraction::new(7, 1, 10))),
+            number::add(expr.infix_token[4], expr.infix_token[6])
+        );
+        assert_eq!(
+            Ok(types::Token::Fraction(types::Fraction::new(14, 3, 5))),
+            number::add(expr.infix_token[8], expr.infix_token[10])
+        );
+        assert_eq!(
+            Ok(types::Token::Double(14_f64)),
+            number::add(expr.infix_token[12], expr.infix_token[14])
+        );
+        assert_eq!(
+            Ok(types::Token::Double(10_f64)),
+            number::add(expr.infix_token[16], expr.infix_token[18])
+        );
+        assert_eq!(
+            Ok(types::Token::Double(12.1_f64)),
+            number::add(expr.infix_token[20], expr.infix_token[22])
         );
     }
 }
